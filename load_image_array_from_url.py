164192f4b401189bd705ec93c34d8a2a1c416cf0
cestwc <80936226+cestwc@users.noreply.github.com>
2023-07-22 02:35:39 +0800

    Create load_image_array_from_url.py

---
+++ load_image_array_from_url.py
@@ -0,0 +1,10 @@
+import urllib.request
+import cv2
+import numpy as np
+
+def img_from_url(url):
+	response = urllib.request.urlopen(url)
+	image_data = response.read()
+	np_array = np.frombuffer(image_data, np.uint8)
+	image_bgr = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+	return image_bgr[:, :, ::-1]
