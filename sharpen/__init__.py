a34ab49441c73eabc6d4b3abc977ed3d2c6bd9ba
cestwc <80936226+cestwc@users.noreply.github.com>
2023-10-04 20:35:18 +0800

    Update __init__.py

--- sharpen/__init__.py
+++ sharpen/__init__.py
@@ -1,3 +1,4 @@
 from .load_image_array_from_url import img_from_url
 from .process_bibitem import extract_bibitem_key
 from .time_serial_gen import generate_serial
+from .count_torch_model_parameters import count_parameters
