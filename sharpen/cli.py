8dd997b91dac99d023d917f07c6f54f3a5e06939
cestwc <80936226+cestwc@users.noreply.github.com>
2025-06-26 16:29:58 +0800

    Update cli.py

--- sharpen/cli.py
+++ sharpen/cli.py
@@ -1,4 +1,6 @@
 import argparse
+import getpass
+
 from .push_image_dataset_to_hub import push_images
 
 def main():
@@ -10,8 +12,8 @@ def main():
 	push.add_argument("--images", required=True, help="Folder, tensor, or array input")
 	push.add_argument("--labels", required=True, help="Label list or .parquet path")
 	push.add_argument("--repo", required=True, help="HF dataset repo name")
-	push.add_argument("--token", required=True, help="HF access token")
-	push.add_argument("--config", required=True, help="Dataset config/version")
+	# push.add_argument("--token", required=True, help="HF access token")
+	push.add_argument("--config-name", required=True, help="Dataset config/version")
 	push.add_argument("--class-names", default='', help="Class names: list, 'cifar10', or HF dataset")
 	push.add_argument("--private", action="store_true", help="Make dataset private")
 	push.add_argument("--image-sort-mode", default="natural", help="Sort mode: natural/plain/mtime/none")
@@ -19,18 +21,20 @@ def main():
 	args = parser.parse_args()
 	
 	if args.command == "push-images":
+		token = args.token or getpass.getpass("Enter Hugging Face token: ")
+		
 		# Load labels from .txt if applicable
-		label_source = args.labels
-		if isinstance(label_source, str) and label_source.endswith(".txt"):
-			with open(label_source) as f:
-				label_source = [int(x.strip()) for x in f]
+		labels = args.labels
+		if isinstance(labels, str) and labels.endswith(".txt"):
+			with open(labels) as f:
+				labels = [int(x.strip()) for x in f]
 		
 		push_images(
-			X=args.images,
-			dataset_repo=args.repo,
-			token=args.token,
-			config_name=args.config,
-			label_source=label_source,
+			images=args.images,
+			repo=args.repo,
+			token=token,
+			config_name=args.config_name,
+			labels=labels,
 			class_names=args.class_names,
 			private=args.private,
 			image_sort_mode=args.image_sort_mode
