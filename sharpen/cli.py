07e97ef9d0bfb17862999404138bd3bbb88c426f
cestwc <80936226+cestwc@users.noreply.github.com>
2025-12-31 00:07:24 +0800

    Update cli.py

--- sharpen/cli.py
+++ sharpen/cli.py
@@ -20,7 +20,7 @@ def main():
 	push.add_argument("--image-sort-mode", default="natural", help="Sort mode: natural/plain/mtime/none")
 
 	# Subcommand: push
-	dvips = subparsers.add_parser("dvips-match", help="Approximation of a target Hex color using dvipsnames")
+	dvips = subparsers.add_parser("dvips", help="Approximation of a target Hex color using dvipsnames")
 	dvips.add_argument("hex", help="Target Hex Code (e.g. #3450a0)")
 	dvips.add_argument("-n", "--bangs", type=int, default=2, help="Max depth of mixing (default: 2)")
 	dvips.add_argument("-m", "--metric", choices=['rgb', 'lab'], default='rgb', 
@@ -55,7 +55,7 @@ def main():
 	
 	
 	
-	if args.command == "dvips-match":
+	if args.command == "dvips":
 		
 		print(f"Target: {args.hex}")
 		print(f"Config: n={args.bangs}, beam={args.beam}, step={args.step}")
